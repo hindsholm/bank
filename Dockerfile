93c84e5c63ccb919eff1af0fe1ebc1077fcd078a
Morten Hindsholm <morten@hindsholm.dk>
2018-02-22 14:03:16 +0100

    Integration test now starts and stops container

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
-FROM airhacks/payara-micro
-ENV ARCHIVE_NAME bank.war
-COPY ./target/bank.war ${DEPLOYMENT_DIR}
+FROM payara/micro
+COPY ./target/bank.war /opt/payara/deployments
+ENTRYPOINT []
+CMD java -jar /opt/payara/payara-micro.jar --deploymentDir /opt/payara/deployments
+
+# FROM airhacks/payara-micro
+# ENV ARCHIVE_NAME bank.war
+# COPY ./target/bank.war ${DEPLOYMENT_DIR}
--- src/test/java/dk/hindsholm/bank/AccountExposureIT.java
+++ src/test/java/dk/hindsholm/bank/AccountExposureIT.java
@@ -6,41 +6,52 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Ignore;
 
 import org.junit.Test;
+import pl.domzal.junit.docker.rule.DockerRule;
+import pl.domzal.junit.docker.rule.WaitFor;
 
-@SuppressWarnings("unchecked")
 public class AccountExposureIT {
-    
+
+    @ClassRule
+    public static DockerRule container = DockerRule.builder()
+            .imageName("bank")
+            .waitFor(WaitFor.logMessage("bank was successfully deployed"))
+            .build();
+
     WebTarget target;
-    
+
     @Before
     public void setup() {
-        target = ClientBuilder.newClient().target("http://localhost:8080/bank/resources");
+        String dockerUrl = "http://" + container.getDockerHost() + ":" + container.getExposedContainerPort("8080") + "/bank/resources";
+        target = ClientBuilder.newClient().target(dockerUrl);
     }
 
     @Test
     public void testListAccounts() {
-        Map<String, Object> response = target.path("accounts")
+        Map response = target.path("accounts")
                 .request()
                 .accept("application/hal+json")
                 .get(Map.class);
 
         assertNotNull(response.get("_embedded"));
-        Map<String, Object> embedded = (Map<String, Object>) response.get("_embedded");
+        Map embedded = (Map) response.get("_embedded");
         assertTrue(((List) embedded.get("accounts")).size() >= 2);
     }
 
     @Test
     public void testGetAccount() {
-        Map<String, Object> response = target.path("accounts").path("5479-1234567")
+        Map response = target.path("accounts").path("5479-1234567")
                 .request()
                 .accept("application/hal+json")
                 .get(Map.class);
@@ -59,7 +70,7 @@ public class AccountExposureIT {
         accountCreate.put("regNo", "5479");
         accountCreate.put("accountNo", Integer.toString(accountNo));
         accountCreate.put("name", "Savings account");
-        Map<String, Object> response = target.path("accounts").path("5479-" + accountNo)
+        Map response = target.path("accounts").path("5479-" + accountNo)
                 .request()
                 .accept("application/hal+json")
                 .put(Entity.entity(accountCreate, MediaType.APPLICATION_JSON_TYPE), Map.class);
@@ -76,7 +87,7 @@ public class AccountExposureIT {
         accountCreate.put("regNo", "5479");
         accountCreate.put("accountNo", "1234567");
         accountCreate.put("name", "new account name");
-        Map<String, Object> response = target.path("accounts").path("5479-" + "1234567")
+        Map response = target.path("accounts").path("5479-" + "1234567")
                 .request()
                 .accept("application/hal+json")
                 .put(Entity.entity(accountCreate, MediaType.APPLICATION_JSON_TYPE), Map.class);
