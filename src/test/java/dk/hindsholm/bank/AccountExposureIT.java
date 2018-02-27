f07ef7a1433a0f420cbf4e348ddf0f9ff3880c7d
Morten Hindsholm <morten@hindsholm.dk>
2018-02-27 14:46:51 +0100

    Added a test and cleaned up

--- src/test/java/dk/hindsholm/bank/AccountExposureIT.java
+++ src/test/java/dk/hindsholm/bank/AccountExposureIT.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.NotAuthorizedException;
 
 import javax.ws.rs.client.ClientBuilder;
@@ -42,7 +43,6 @@ public class AccountExposureIT {
                 .request()
                 .accept("application/hal+json")
                 .get(Map.class);
-
         assertNotNull(response.get("_embedded"));
         Map embedded = (Map) response.get("_embedded");
         assertTrue(((List) embedded.get("accounts")).size() >= 2);
@@ -55,14 +55,13 @@ public class AccountExposureIT {
                 .request()
                 .accept("application/hal+json")
                 .get(Map.class);
-
         assertEquals("5479", response.get("regNo"));
         assertEquals("1234567", response.get("accountNo"));
         assertEquals("Checkings Account", response.get("name"));
     }
 
     @Test(expected = NotAuthorizedException.class)
-    public void testNotAuthorized() {
+    public void testUnknownUser() {
         Map<String, String> accountCreate = new HashMap<>();
         accountCreate.put("regNo", "5479");
         accountCreate.put("accountNo", "123456");
@@ -74,20 +73,31 @@ public class AccountExposureIT {
                 .put(Entity.entity(accountCreate, MediaType.APPLICATION_JSON_TYPE), Map.class);
     }
 
+    @Test(expected = ForbiddenException.class)
+    public void testUserNotInRequiredGroup() {
+        Map<String, String> accountCreate = new HashMap<>();
+        accountCreate.put("regNo", "5479");
+        accountCreate.put("accountNo", "123456");
+        accountCreate.put("name", "Savings account");
+        WebTarget target = ClientBuilder.newClient().register(new Authenticator("cust1", "passw0rd")).target(dockerUrl);
+        target.path("accounts").path("dummy")
+                .request()
+                .accept("application/json")
+                .put(Entity.entity(accountCreate, MediaType.APPLICATION_JSON_TYPE), Map.class);
+    }
+
     @Test
     public void testCreateAccount() {
         int accountNo = ThreadLocalRandom.current().nextInt(9999999);
-
+        WebTarget target = ClientBuilder.newClient().register(new Authenticator("adv1", "passw0rd")).target(dockerUrl);
         Map<String, String> accountCreate = new HashMap<>();
         accountCreate.put("regNo", "5479");
         accountCreate.put("accountNo", Integer.toString(accountNo));
         accountCreate.put("name", "Savings account");
-        WebTarget target = ClientBuilder.newClient().register(new Authenticator("adv1", "passw0rd")).target(dockerUrl);
         Map response = target.path("accounts").path("5479-" + accountNo)
                 .request()
                 .accept("application/hal+json")
                 .put(Entity.entity(accountCreate, MediaType.APPLICATION_JSON_TYPE), Map.class);
-
         assertEquals("5479", response.get("regNo"));
         assertEquals(Integer.toString(accountNo), response.get("accountNo"));
         assertEquals("Savings account", response.get("name"));
@@ -95,11 +105,11 @@ public class AccountExposureIT {
 
     @Test
     public void testUpdateAccount() {
+        WebTarget target = ClientBuilder.newClient().register(new Authenticator("adv1", "passw0rd")).target(dockerUrl);
         Map<String, String> accountUpdate = new HashMap<>();
         accountUpdate.put("regNo", "5479");
         accountUpdate.put("accountNo", "1234567");
         accountUpdate.put("name", "new account name");
-        WebTarget target = ClientBuilder.newClient().register(new Authenticator("adv1", "passw0rd")).target(dockerUrl);
         Map response = target.path("accounts").path("5479-" + "1234567")
                 .request()
                 .accept("application/hal+json")
