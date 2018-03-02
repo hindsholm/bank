a0210b2f7f4033a2816a50adc4002f6ac9a27f82
Morten Hindsholm <morten@hindsholm.dk>
2018-03-02 10:17:50 +0100

    Cleanup

--- src/main/java/dk/hindsholm/bank/boundary/AccountUpdate.java
+++ src/main/java/dk/hindsholm/bank/boundary/AccountUpdate.java
@@ -5,38 +5,37 @@ import javax.validation.constraints.Pattern;
 
 public class AccountUpdate {
 
-    @Pattern(regexp = "^[0-9]{4}$")
+    @Pattern(regexp = "^\\d{4}$")
     private String regNo;
 
-    @Pattern(regexp = "^[0-9]+$")
+    @Pattern(regexp = "^\\d+$")
     private String accountNo;
 
     @NotNull
-    @Pattern(regexp = ".{1,40}")
     private String name;
-
-    public String getRegNo() {
-        return regNo;
-    }
-
-    public String getAccountNo() {
-        return accountNo;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     public void setRegNo(String regNo) {
         this.regNo = regNo;
+    }
+
+    public String getRegNo() {
+        return regNo;
     }
 
     public void setAccountNo(String accountNo) {
         this.accountNo = accountNo;
     }
 
+    public String getAccountNo() {
+        return accountNo;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
     
+    public String getName() {
+        return name;
+    }
+
 }
